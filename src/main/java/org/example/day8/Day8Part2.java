c98b50ecfc2f5d3bbf7e86026a366dcf129e5fef
AaronMacF <aaron.j.macfarlane2@gmail.com>
2025-12-10 17:13:26 +0000

    Day 8 Part 2 improvements - inc. using UnionFind

--- src/main/java/org/example/day8/Day8Part2.java
+++ src/main/java/org/example/day8/Day8Part2.java
@@ -1,52 +1,41 @@
 package org.example.day8;
 
-import com.google.common.collect.Sets;
 import org.example.Day;
+import org.example.shared.Pair;
 import org.example.utils.FileUtilities;
+import org.jgrapht.alg.util.UnionFind;
 
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 
 @SuppressWarnings("unused")
 public class Day8Part2 implements Day {
     public void run() {
         List<String> input = FileUtilities.readPuzzleInput("day8.txt");
-        Set<Box> boxes = input
+        List<Box> boxes = input
             .stream()
             .map(s -> s.split(","))
             .map(coords -> new Box(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]), Integer.parseInt(coords[2])))
-            .collect(Collectors.toSet());
+            .toList();
 
-        Set<Set<Box>> chains = boxes
-            .stream()
-            .map(b -> new HashSet<>(Set.of(b)))
-            .collect(Collectors.toSet());
+        UnionFind<Box> chains = new UnionFind<>(new HashSet<>(boxes));
 
-        Set<Set<Box>> combos = Sets.combinations(boxes, 2);
-        var closestBoxes = combos.stream().sorted(Comparator.comparingDouble(this::straightLineDistance)).toList();
-        var closestBoxesIter = closestBoxes.iterator();
+        List<Pair<Box, Box>> boxPairs = new ArrayList<>();
+        for (int i = 0; i < boxes.size(); i ++) {
+            for (int j = i + 1; j < boxes.size(); j++) {
+              boxPairs.add(new Pair<>(boxes.get(i), boxes.get(j)));
+            }
+        }
+        boxPairs.sort(Comparator.comparingDouble(this::straightLineDistance));
+        var boxPairsIter = boxPairs.iterator();
 
         Box lastBox1 = null;
         Box lastBox2 = null;
-        while (chains.size() > 1)
+        while (chains.numberOfSets() > 1)
         {
-            var nextPair = closestBoxesIter.next();
-            Box[] boxesArr = nextPair.toArray(new Box[2]);
-            var box1 = boxesArr[0];
-            var box2 = boxesArr[1];
-            var setA = chains.stream().filter(s -> s.contains(box1)).findFirst().orElseThrow();
-            var setB = chains.stream().filter(s -> s.contains(box2)).findFirst().orElseThrow();
-            if (setA.equals(setB)) {
-                continue;
-            }
-            chains.remove(setA);
-            chains.remove(setB);
-            var union = new HashSet<>(setA);
-            union.addAll(setB);
-            chains.add(union);
+            var nextPair = boxPairsIter.next();
+            var box1 = nextPair.first();
+            var box2 = nextPair.second();
+            chains.union(box1, box2);
             lastBox1 = box1;
             lastBox2 = box2;
         }
@@ -59,10 +48,9 @@ public class Day8Part2 implements Day {
 
     }
 
-    private double straightLineDistance(Set<Box> boxes) {
-        Box[] boxesArr = boxes.toArray(new Box[2]);
-        var box1 = boxesArr[0];
-        var box2 = boxesArr[1];
+    private double straightLineDistance(Pair<Box, Box> pair) {
+        var box1 = pair.first();
+        var box2 = pair.second();
         return Math.sqrt(Math.pow(box1.x - box2.x, 2) + Math.pow(box1.y - box2.y, 2) + Math.pow(box1.z - box2.z, 2));
     }
 }
